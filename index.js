a3e07e8b7822761d0c7c7c2d4af0269dac10d556
Joseph Podwys <joseph@podwys.com>
2020-07-01 12:58:16 -0600

    Trying out a new way to use Observer

--- index.js
+++ index.js
@@ -16,33 +16,52 @@ function resolveArgs (store, component) {
   return [ store, component ];
 }
 
-// export function Observer (...args) {
+// This implementation lacks forwardRef and accesses every state prop on every render.
+// It also cannot tell the difference between props prop a parent component, a ref, and store.state.
+// (If I decide to compute derived state eagerly then accessing all state on every render is no longer a problem.)
+// export const Observer = (...args) => {
+//   return (props, ref) => {
+//     const [ store, component ] = resolveArgs(...args);
+//     const render = useState();
+//     const subscriber = component.prototype && component.prototype.render ? (new component()).render : component;
+//     const [ deps, output ] = reflect({ ...props, ref, state: store.state, fire: store.fire }, subscriber);
+//     const unsubscribe = store.subscribe(Array.from(deps), () => render[1]({}));
+//     useEffect(() => () => unsubscribe(), [ unsubscribe ]);
+//     return output;
+//   };
+// }
+
+// This implementation uses forwardRef but required the user to pass an array of state props as the first argument.
+// export const Observer = (...args) => {
+//   const deps = args.shift();
 //   const [ store, component ] = resolveArgs(...args);
-//   function Wrapper() {
-//     let unsubscribe;
-//     this.componentDidMount = () => {
-//       const subscriber = component.prototype && component.prototype.render ? (new component()).render : component;
-//       unsubscribe = store.subscribe(subscriber, () => this.setState({}));
-//     };
-//     this.componentWillUnmount = () => unsubscribe();
-//     this.render = (props) => h(component, { ...props, ...store.state, fire: store.fire });
-//   }
-//   (Wrapper.prototype = new Component()).constructor = Wrapper;
-//   return forwardRef((originProps, ref) => { // eslint-disable-line react/display-name
-//     return h(Wrapper, { ...originProps, ...store.state, fire: store.fire, ref });
+//   return forwardRef((originProps, ref) => {
+//     const render = useState();
+//     useEffect(() => store.subscribe(deps, () => render[1]({})), []);
+//     const props = { ...originProps, ref, fire: store.fire };
+//     deps.forEach((dep) => props[dep] = store.state[dep]);
+//     return h(component, props);
 //   });
 // }
 
-// This does not use forwardRef like the above implementation. I need to look into this.
+// This eimplementation uses forwardRef, does not require a list of arguments, and does not access all state on each render.
+// However, it does require that state be accessed via a prop called `state` rather than spreading the desired values onto `props`.
+// This comes with the side-effect of allowing users to directly manipulate state within components.
 export const Observer = (...args) => {
-  return memo((props) => {
+  return forwardRef((props, ref) => {
     const [ store, component ] = resolveArgs(...args);
     const render = useState();
-    const subscriber = component.prototype && component.prototype.render ? (new component()).render : component;
-    // This line is inneficient because it runs all computed props on every render.
-    // I can optimize this by only running reflect on the first render.
-    // After that, I already have deps. I can use deps to only read the required props from state.
-    const [ deps, output ] = reflect({ ...props, ...store.state, fire: store.fire }, subscriber);
+    const subscriber = component.prototype && component.prototype.render
+      ? (new component()).render
+      : component;
+    const deps = new Set();
+    const state = new Proxy(store.state, {
+      get (obj, prop) {
+        deps.add(prop);
+        return obj[prop];
+      }
+    })
+    const output = subscriber({ ...props, ref, state, fire: store.fire });
     const unsubscribe = store.subscribe(Array.from(deps), () => render[1]({}));
     useEffect(() => () => unsubscribe(), [ unsubscribe ]);
     return output;
